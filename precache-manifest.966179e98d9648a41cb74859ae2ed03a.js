b4166d78428a097fd6d1c498a24c228a8a341cbe
pamu <nagarjuna.pamu@gmail.com>
2020-06-17 21:32:17 +0000

    Updates

---
+++ precache-manifest.966179e98d9648a41cb74859ae2ed03a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3eb6eaf312d2bf065ee304ad2599df5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62f14e9f69619a38ac61",
+    "url": "/static/css/main.1518c80d.chunk.css"
+  },
+  {
+    "revision": "e56280469225d39f489b",
+    "url": "/static/js/2.4febcda6.chunk.js"
+  },
+  {
+    "revision": "e5999e63c33a32e77a192dc439bcb94e",
+    "url": "/static/js/2.4febcda6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f14e9f69619a38ac61",
+    "url": "/static/js/main.7c913c5e.chunk.js"
+  },
+  {
+    "revision": "ea039d9e12dcb107a864",
+    "url": "/static/js/runtime-main.f8d3ebe6.js"
+  },
+  {
+    "revision": "d17780e6381008a8759ea3eed30eda63",
+    "url": "/static/media/cheetah.d17780e6.png"
+  }
+]);
